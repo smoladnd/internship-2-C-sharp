e107ae5fdd400018544f8dfd476b07e93d0c4b87
smoladnd <tomica.smola@gmail.com>
2021-11-17 15:06:22 +0100

    Created dictonary with 11 entries

--- Popis stanovnika/Program.cs
+++ Popis stanovnika/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Popis_stanovnika
 {
@@ -6,6 +7,22 @@ namespace Popis_stanovnika
     {
         static void Main(string[] args)
         {
+            var populationList = new Dictionary<string, (string nameAndSurname, DateTime dateOfBirth)>()
+            {
+                {"02526111191", ("Andrea Perković", new DateTime(1942, 03, 19)) },
+                {"95305346132", ("Dario Babić", new DateTime (1947, 01, 20)) },
+                {"60329579242", ("Mandica Perko",new DateTime (1948, 01, 21)) },
+                {"06075173965", ("Danica Vinković", new DateTime (1950, 07, 05)) },
+                {"02368243560", ("Rozalija Babić", new DateTime (1961, 03, 03)) },
+                {"50453718074", ("Bogdan Stojanović", new DateTime (1965, 09, 15)) },
+                {"17354691849", ("Martina Tomić", new DateTime (1965,  10, 30)) },
+                {"05908685970", ("Igor Pavlović", new DateTime (1983, 06, 03)) },
+                {"61750696186", ("Lana Marić", new DateTime (1997, 09, 07)) },
+                {"69670149194", ("Lucija Milić", new DateTime (1999, 04, 25)) },
+                {"20234601029", ("Barbara Jugovac", new DateTime (2005, 10, 28)) }
+            };
+
+
             Console.WriteLine("Odaberite akciju:");
             Console.WriteLine("1 - Ispis stanovnistva");
             Console.WriteLine("2 - Ispis stanovnika po OIB-u");
@@ -17,6 +34,7 @@ namespace Popis_stanovnika
             Console.WriteLine("8 - Uredivanje stanovnika");
             Console.WriteLine("9 - Statiskika");
             Console.WriteLine("0 - Izlaz iz aplikacije");
+
         }
     }
 }
