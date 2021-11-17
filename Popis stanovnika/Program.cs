e44ac36669ac47dc3a746ed4756d8657ed70b109
smoladnd <tomica.smola@gmail.com>
2021-11-17 13:11:51 +0100

    Initializing main user interface with 10 options

---
+++ Popis stanovnika/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Popis_stanovnika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odaberite akciju:");
+            Console.WriteLine("1 - Ispis stanovnistva");
+            Console.WriteLine("2 - Ispis stanovnika po OIB-u");
+            Console.WriteLine("3 - Ispis OIB-a po unosu imena i prezimena");
+            Console.WriteLine("4 - Unos novog stanovnika");
+            Console.WriteLine("5 - Brisanje stanovnika po OIB-u");
+            Console.WriteLine("6 - Brisanje stanovnika po imenu i prezimenu te datumu rodenja");
+            Console.WriteLine("7 - Brisanje svih stanovnika");
+            Console.WriteLine("8 - Uredivanje stanovnika");
+            Console.WriteLine("9 - Statiskika");
+            Console.WriteLine("0 - Izlaz iz aplikacije");
+        }
+    }
+}
